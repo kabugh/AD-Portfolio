e844c7dcc4bd2d9f9712e52644fe544e9f5b09a7
kabugh <kabugh00@gmail.com>
2020-07-24 17:52:28 +0200

    Services contentful update

--- src/store/modules/services.ts
+++ src/store/modules/services.ts
@@ -8,22 +8,52 @@ const client = contentful.createClient({
 });
 
 const state = {
+  services: [] as Service[],
   currentService: {} as Service
 };
 
 const mutations = {
+  setServices(state: { services: Service[] }, payload: any) {
+    state.services = payload;
+  },
   setCurrentService(state: { currentService: Service }, payload: any) {
     state.currentService = payload;
   }
 };
 
 const getters = {
+  services(state: { services: any }) {
+    return state.services;
+  },
   currentService(state: { currentService: Service }) {
     return state.currentService;
   }
 };
 
 const actions = {
+  fetchServices({ commit, state }: any) {
+    if (state.services.length === 0) {
+      commit("setLoading", true);
+      commit("clearError");
+      client
+        .getEntries({
+          order: "sys.createdAt",
+          /* eslint-disable @typescript-eslint/camelcase */
+          content_type: "offers",
+          include: 2 // specified level of references, links
+        })
+        .then((entries: { items: any[] }) => {
+          const services = entries.items[0].fields.offersContainer.map(
+            (a: any) => a.fields
+          );
+          console.log(services);
+          commit("setServices", services);
+          commit("setLoading", false);
+        });
+    } else {
+      commit("setServices", state.services);
+    }
+  },
   fetchService({ commit, state }: any, slug: string) {
     if (state.currentService) {
       if (Object.values(state.currentService).length === 0) {
